f9eb4cde85b31c2bc391c0a62bafca0b70c569b7
Zhexuan Chen <2915234902@qq.com>
2023-03-12 13:01:46 +0800

    Start git-commit-mode in insert state if in a blank commit message

--- src/osf-vc.el
+++ src/osf-vc.el
@@ -26,4 +26,14 @@
 
 (straight-use-package 'magit)
 
+(defun osf-git-commit-start-in-insert-state-if-blank ()
+  "Start git-commit-mode in insert state if in a blank commit message,
+otherwise in default state."
+  (when (and (bound-and-true-p evil-mode)
+             (not (evil-emacs-state-p))
+             (bobp) (eolp))
+    (evil-insert-state)))
+(add-hook 'git-commit-setup-hook
+          #'osf-git-commit-start-in-insert-state-if-blank)
+
 (provide 'osf-vc)
