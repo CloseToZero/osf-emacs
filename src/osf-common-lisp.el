7e89708a33bc87b027e7f12b24557ea1b5fef00c
Zhexuan Chen <2915234902@qq.com>
2023-03-27 00:00:51 +0800

    [common-lisp] Add commands to eval exp/defun at repl but keep in same window

--- src/osf-common-lisp.el
+++ src/osf-common-lisp.el
@@ -39,10 +39,66 @@
   (let ((local-hyperspec-dir (expand-file-name "~/HyperSpec/")))
     (when (file-directory-p local-hyperspec-dir)
       (setq common-lisp-hyperspec-root
-            (concat "file:///" local-hyperspec-dir)))))
+            (concat "file:///" local-hyperspec-dir))))
+
+  (defun osf-slime-last-expression ()
+    (if (and (not evil-move-beyond-eol)
+             (or (evil-normal-state-p) (evil-motion-state-p)))
+        (save-excursion
+          (unless (or (eobp) (eolp)) (forward-char))
+          (slime-last-expression))
+      (slime-last-expression)))
+
+  (defun osf-gen-slime-eval-in-repl-fn (form-fn)
+    "Return a function like `slime-eval-last-expression-in-repl',
+but keep in the same window.
+The returned function call FORM-FN to get the form to be evaluated."
+    (lambda (prefix)
+      (interactive "P")
+      (let ((form (funcall form-fn))
+            (buffer-name (buffer-name (current-buffer)))
+            (new-package (slime-current-package))
+            (old-package (slime-lisp-package))
+            (slime-repl-suppress-prompt t)
+            (yank-back nil))
+        (with-current-buffer (slime-output-buffer)
+          (unless (eq (current-buffer) (window-buffer))
+            (display-buffer (current-buffer) '(display-buffer-reuse-window)))
+          (goto-char (point-max))
+          ;; Kill pending input in the REPL
+          (when (< (marker-position slime-repl-input-start-mark) (point))
+            (kill-region slime-repl-input-start-mark (point))
+            (setq yank-back t))
+          (unwind-protect
+              (progn
+                (insert-before-markers (format "\n;;; from %s\n" buffer-name))
+                (when new-package
+                  (slime-repl-set-package new-package))
+                (let ((slime-repl-suppress-prompt nil))
+                  (slime-repl-insert-prompt))
+                (insert form)
+                (slime-repl-return))
+            (unless (or prefix (equal (slime-lisp-package) old-package))
+              ;; Switch back.
+              (slime-repl-set-package old-package)
+              (let ((slime-repl-suppress-prompt nil))
+                (slime-repl-insert-prompt))))
+          ;; Put pending input back.
+          (when yank-back
+            (yank))))))
+
+  (defalias 'osf-slime-eval-last-expression-in-repl
+    (osf-gen-slime-eval-in-repl-fn #'osf-slime-last-expression))
+
+  (defalias 'osf-slime-eval-defun-in-repl
+    (osf-gen-slime-eval-in-repl-fn #'slime-defun-at-point))
+  )
 
 (defun osf--slime-setup-leader-key-bindings (map)
   (osf-local-leader-define-key map
+    "e r e" #'osf-slime-eval-last-expression-in-repl
+    "e r d" #'osf-slime-eval-defun-in-repl
+
     "h f" #'slime-describe-function
     "h s" #'slime-describe-symbol
     "h h" #'slime-documentation-lookup
@@ -71,7 +127,7 @@
   (osf--slime-repl-setup-leader-key-bindings slime-repl-mode-map))
 
 (with-eval-after-load 'slime-repl
-  (defalias #'osf-slime-repl-return-eval-at-end
+  (defalias 'osf-slime-repl-return-eval-at-end
     (osf-lisp-gen-repl-return-eval-at-end-fn #'slime-repl-return))
 
   (defun osf-slime-smart-repl-return ()
