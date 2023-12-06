365f4e41e180cc097c957dd34e8a7aa7568a3315
Zhexuan Chen <2915234902@qq.com>
2023-12-06 16:38:22 +0800

    [saveplace-pdf-view] Fix: lost history after rebuilt pdf-tools

--- src/osf-pdf.el
+++ src/osf-pdf.el
@@ -41,9 +41,50 @@
 (add-hook 'pdf-view-mode-hook #'osf--pdf-view-setup-kill-outline-buffer-hook)
 
 (straight-use-package 'saveplace-pdf-view)
+
 ;; Need enable save-place-mode in the other place.
 (with-eval-after-load 'saveplace
   (with-eval-after-load 'pdf-tools
     (require 'saveplace-pdf-view)))
+
+(with-eval-after-load 'saveplace-pdf-view
+  (defun osf--saveplace-pdf-view-dont-save-if-no-built (fn &rest args)
+    "Don't save the place of pdf-view buffers if pdf-tools haven't been installed.
+Otherwise, we will lose the save-place history of pdf-view buffers."
+    (cond ((derived-mode-p 'pdf-view-mode)
+           (when (ignore-errors (pdf-info-check-epdfinfo) t)
+             (apply fn args)))
+          (t (apply fn args))))
+  (advice-add #'saveplace-pdf-view-to-alist-advice :around
+              #'osf--saveplace-pdf-view-dont-save-if-no-built)
+
+  (defun osf--saveplace-pdf-view-restore-history-after-rebuilt
+      (fn
+       target-directory
+       &optional
+       skip-dependencies-p
+       force-dependencies-p
+       callback
+       build-directory)
+    "Restore pdf-view buffers' save-place history after pdf-tools installed."
+    (apply
+     fn
+     target-directory
+     skip-dependencies-p
+     force-dependencies-p
+     (lambda (executable &rest args)
+       (prog1 (and callback (apply callback executable args))
+         (when executable
+           (dolist (buf (buffer-list))
+             (when (buffer-live-p buf)
+               (with-current-buffer buf
+                 (when (derived-mode-p 'pdf-view-mode)
+                   ;; Switch to buf's window, avoid `pdf-view-bookmark-jump'
+                   ;; switch the buffer of current window to buf.
+                   (with-selected-window (or (get-buffer-window buf) (selected-window))
+                     (save-place-find-file-hook)))))))))
+     build-directory))
+  (advice-add #'pdf-tools-build-server
+              :around #'osf--saveplace-pdf-view-restore-history-after-rebuilt))
 
 (provide 'osf-pdf)
